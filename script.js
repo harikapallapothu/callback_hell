85b36049aed445181b09e46ca1db41bdd2dcb3f0
harikapallapothu <86473670+harikapallapothu@users.noreply.github.com>
2022-11-01 14:26:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 var display = document.createElement('h1');
-        var num =10;
-        var data = function(){
-            if(num>0){
-                
-                display.innerText=num;
-            
-            setTimeout(data,1000)
-            }
-            if(num==0){
-                
-                display.innerText="Happy Independence Day"
-            }
+var num = 10;
+
+setTimeout(()=>{
+    display.innerText = num;
+    num--;
+        setTimeout(()=>{
+            display.innerText = num;
             num--;
-        }
-        setTimeout(data,1000);
-        document.body.append(display);
+            setTimeout(()=>{
+                display.innerText = num;
+                num--;
+                setTimeout(()=>{
+                    display.innerText = num;
+                    num--;
+                    setTimeout(()=>{
+                        display.innerText = num;
+                        num--;
+                        setTimeout(()=>{
+                            display.innerText = num;
+                            num--;
+                            setTimeout(()=>{
+                                display.innerText = num;
+                                num--;
+                                setTimeout(()=>{
+                                    display.innerText = num;
+                                    num--;
+                                    setTimeout(()=>{
+                                        display.innerText = num;
+                                        num--;
+                                        setTimeout(()=>{
+                                            display.innerText = num;
+                                            num--;
+                                            setTimeout(()=>{
+                                                display.innerText = "Happy Independence Day";
+                                                
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+},1000)
+
+document.body.append(display);
+
+
+
